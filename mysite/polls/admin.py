336020926fd86b4386e8aad805e8312ea4a62b4c
f821336459@163.com <f821336459@163.com>
2019-01-28 19:33:20 +0800

    django 20190128

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,7 +1,39 @@
 from django.contrib import admin
-from .models import Question
-
-admin.site.register(Question)
+from .models import Question, Choice
 
 
-# Register your models here.
+# admin.site.register(Question)
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ['pub_date', 'question_text']
+#
+#
+# admin.site.register(Question, QuestionAdmin)
+# class QuestionAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         (None, {'fields': ['question_text']}),
+#         ('Date information', {'fields': ['pub_date']}),
+#     ]
+#
+#
+# admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+
+
+admin.site.register(Question, QuestionAdmin)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,9 +1,7 @@
 from django.db import models
-import datetime
-from django.utils import timezone
+from datetime import datetime
+from pytz import timezone
 
-
-# Create your models here.
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -12,9 +10,13 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
+    # def was_published_recently(self):
+    #     now = timezone.now()
+    #     return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    #
+    # was_published_recently.admin_order_field = 'pub_date'
+    # was_published_recently.boolean = True
+    # was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
